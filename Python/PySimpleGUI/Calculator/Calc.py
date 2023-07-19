e8d23394a5724348319f5a8a4255ccb7a9afb510
Abeeth S <abeeth.works@gmail.com>
2023-07-19 14:05:55 +0530

    calc gui + theming set

--- Python/PySimpleGUI/Calculator/Calc.py
+++ Python/PySimpleGUI/Calculator/Calc.py
@@ -1,64 +1,28 @@
 import PySimpleGUI as psg
+import Calc_functions as cf
 
 # got carried away - tried to automate creating the buttons: throws below
 # AttributeError: 'str' object has no attribute 'ParentContainer'
 # def b_dropper(face):
 #     return f"psg.Button('{face}', key={face})"
 
-psg.theme('LightGrey1')  # do this before elements are created
-psg.set_options(font='Franklin 15', button_element_size=(3,2))
-
-# psg.Push() will do it in a simpler way [[psg.Push(), calc_display]...]...
-calc_display = psg.Text('0', enable_events=True, font='Roboto 40', justification='right', expand_x=True,
-                        pad=(10, 20), key='calc_display')
-
-zero = psg.Button('0', key='0')
-one = psg.Button('1', key='1')
-two = psg.Button('2', key='2')
-three = psg.Button('3', key='3')
-four = psg.Button('4', key='4')
-five = psg.Button('5', key='5')
-six = psg.Button('6', key='6')
-seven = psg.Button('7', key='7')
-eight = psg.Button('8', key='8')
-nine = psg.Button('9', key='9')
-
-plus = psg.Button('+', key='+', expand_x=True)
-minus = psg.Button('-', key='-')
-multip = psg.Button('*', key='*')
-div = psg.Button('/', key='/')
-
-equals = psg.Button('=', key='=', expand_x=True)
-clear = psg.Button('CE', key='CE', button_color='red')
-dec = psg.Button('.', key='.')
-toggle = psg.Spin(['light', 'dark'], key='toggle', enable_events=True, expand_x=True)
-humble_brag = psg.Text(':: Calc v1.0 ::')
-
-layout = [[calc_display],
-          [toggle, humble_brag],
-          [equals, clear],
-          [seven, eight, nine, div],
-          [four, five, six, multip],
-          [one, two, three, minus],
-          [zero, dec, plus]]
-
-calc_window = psg.Window('Calculator1.0', layout)
+menu = ['menu', ['LightGrey1', 'DarkGray8', 'random']]
+calc_window = cf.create_calc('LightGrey1', menu)
 
 while True:
     event, values = calc_window.read()
 
-    # remove after testing
-    print(f"\n{event} ::: Events logged"
-          f"\n{values} ::: Values captured"
-          f"\n --- --- --- ---")
-    # remove after testing
+    # REMOVE AFTER TESTING
+    cf.test_function(event, values, calc_window)
+    # REMOVE AFTER TESTING
+
+    # Theme settings
+    if event in menu[1]:
+        print(event)
 
     if event == psg.WINDOW_CLOSED:
         break
     if event == '7':
-        # grab existing box value and store it
-        # what_was = values['calc_display']
-        # print(what_was)
-        # START HERE 
         continue
+
 calc_window.close()
--- Python/PySimpleGUI/Calculator/Calc_functions.py
+++ Python/PySimpleGUI/Calculator/Calc_functions.py
@@ -1,1 +1,33 @@
-#
+import PySimpleGUI as psg
+
+
+def test_function(event, values, window_name):
+    window_name['log'].update(f"{event} | {values}")
+    print(f"Event - {event} ||| Values - {values}")
+
+
+def create_calc(theme, menu):
+    psg.theme(theme)  # do this before elements are created
+    psg.set_options(font='Franklin 15', button_element_size=(3, 2))
+
+    # psg.Push() will do it in a simpler way [[psg.Push(), calc_display]...]... vs pad=(x,y)
+
+    layout = [[psg.Text('0',
+                        enable_events=True,
+                        font='Roboto 40',
+                        justification='right',
+                        expand_x=True,
+                        pad=(10, 20),
+                        key='calc_display',
+                        right_click_menu=menu)],
+              # [psg.Spin(['light', 'dark'],
+              #           key='toggle',
+              #           enable_events=True,
+              #           expand_x=True),
+              [psg.Text('', key='log')],
+              [psg.Button('=', key='=', expand_x=True), psg.Button('CE', key='CE', button_color='red')],
+              [psg.Button('7', key='7'), psg.Button('8', key='8'), psg.Button('9', key='9'), psg.Button('/', key='/')],
+              [psg.Button('4', key='4'), psg.Button('5', key='5'), psg.Button('6', key='6'), psg.Button('*', key='*')],
+              [psg.Button('1', key='1'), psg.Button('2', key='2'), psg.Button('3', key='3'), psg.Button('-', key='-')],
+              [psg.Button('0', key='0'), psg.Button('.', key='.'), psg.Button('+', key='+', expand_x=True)]]
+    return psg.Window('Calc v1.0', layout)
