ff1449e6327102eff5cd2dd02a4549febc573f39
Abeeth S <abeeth.works@gmail.com>
2023-07-27 20:51:04 +0530

    pdf dir setup

--- Python/XLSX/openpyxl_code.py
+++ Python/XLSX/openpyxl_code.py
@@ -1,16 +1,9 @@
 import openpyxl
 
-# 1. specify filepath(s)
-path = 'openpyxl_data.xlsx'
-
-# 2. create workbook object using .load_workbook(path)
-workbook_object = openpyxl.load_workbook(path)
-
-# 3. get active sheet from workbook object
-active_sheet_object = workbook_object.active
-
-# 4. cell object has row, column and coordinate attributes
-selected_cell_object = active_sheet_object.cell(row=1, column=1)
+path = 'openpyxl_data.xlsx'  # 1. specify filepath(s)
+workbook_object = openpyxl.load_workbook(path)  # 2. create workbook object using .load_workbook(path)
+active_sheet_object = workbook_object.active  # 3. get active sheet from workbook object
+selected_cell_object = active_sheet_object.cell(row=1, column=1)  # 4. cell object has row, column and coordinate attributes
 
 cell_value = f"Values extracted from the select cell object are: {selected_cell_object.value}"
 print(cell_value)
@@ -21,8 +14,8 @@ print(rows_value)
 columns_value = f"Number of columns in the sheet at {path} : {active_sheet_object.max_column}"
 print(columns_value)
 
-# Get all column names
-number_of_columns = active_sheet_object.max_column
+number_of_columns = active_sheet_object.max_column  # 4. cell object has row, column and coordinate attributes
 for col_num in range(1, number_of_columns + 1):
     cell_object = active_sheet_object.cell(row=1, column=col_num)
     print(cell_object)
+
